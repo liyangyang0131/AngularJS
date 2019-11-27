31b78fd4c669bb8f2f62948b930de1dcf4f27fd2
pengfeihong <pengfeihong@bis.com.cn>
2019-11-27 15:35:39 +0800

    路由修改

--- src/route.js
+++ src/route.js
@@ -1,6 +1,7 @@
 app.config(['$routeProvider',function($routeProvider){
     $routeProvider
-    .when('/note',{templateUrl:'./views/note.html',controller:'noteController'})
-    .when('/memo',{templateUrl:'./views/memo.html',controller:'memoController'})
-    .otherwise({redirect:'/note'})
+        .when('/',{redirectTo:'/note'})
+        .when('/note',{templateUrl:'./views/note.html',controller:'noteController'})
+        .when('/memo',{templateUrl:'./views/memo.html',controller:'memoController'})
+        .otherwise({redirectTo:'/'});
 }]);
